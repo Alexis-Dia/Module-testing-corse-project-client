4184ea59ab7a8f21e7b12880e7ddf948ab477328
Alexis-Dk <alexeydruzik@inbox.ru>
2020-03-11 18:47:57 +0300

    Refactoring.

--- src/routes/tasks/components/tasksView/TasksView.js
+++ src/routes/tasks/components/tasksView/TasksView.js
@@ -70,7 +70,7 @@ class TasksView extends Component {
     }
   }
 
-  componentWillReceiveProps(nextpropsl) {
+  componentWillReceiveProps(nextprops) {
     if (nextprops.auth !== this.props.auth) {
       if (nextprops.auth.isAuthenticated) {
         if (nextprops.auth.user.userRole === 'USER') {
